6bd9d0c8c6fba0d3cbfeb8f0bbbba62f165ced7a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-18 22:09:06 -0300

    fix: sort null exception in get transactions

--- src/main/java/br/com/dio/picpayclone/infrastructure/persistence/mappers/PageableMapper.java
+++ src/main/java/br/com/dio/picpayclone/infrastructure/persistence/mappers/PageableMapper.java
@@ -8,18 +8,21 @@ import org.springframework.data.domain.Sort;
 public class PageableMapper {
 
     public static Pageable toEntity(PageableDTO dto) {
-        var sort = Sort.by(dto.getSort().stream().map(sortString -> {
-            String[] splitSortString = sortString.split(",");
-            String property = splitSortString[0];
-            String orderString = splitSortString[1];
-            Sort.Direction direction = null;
-            if (!orderString.isEmpty()) {
-                orderString = orderString.toUpperCase();
-                if (orderString.equals("ASC")) direction = Sort.Direction.ASC;
-                else if (orderString.equals("DESC")) direction = Sort.Direction.DESC;
-            }
-            return new Sort.Order(direction, property);
-        }).toList());
+        Sort sort = Sort.unsorted();
+        if (dto.getSort() != null) {
+            sort = Sort.by(dto.getSort().stream().map(sortString -> {
+                String[] splitSortString = sortString.split(",");
+                String property = splitSortString[0];
+                String orderString = splitSortString[1];
+                Sort.Direction direction = null;
+                if (!orderString.isEmpty()) {
+                    orderString = orderString.toUpperCase();
+                    if (orderString.equals("ASC")) direction = Sort.Direction.ASC;
+                    else if (orderString.equals("DESC")) direction = Sort.Direction.DESC;
+                }
+                return new Sort.Order(direction, property);
+            }).toList());
+        }
         return PageRequest.of(dto.getPage(), dto.getSize(), sort);
     }
 
